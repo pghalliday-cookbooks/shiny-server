70724806965f7b661f1f18210f7a067133dd1b14
Peter Halliday <pghalliday@gmail.com>
2015-03-10 00:08:57 +0100

    git sync should be outside of application resource

--- providers/application.rb
+++ providers/application.rb
@@ -6,30 +6,19 @@ use_inline_resources
 
 action :add do
   name = new_resource.name
-  repository = new_resource.repository
-  sub_folder = new_resource.sub_folder
+  path = new_resource.path
   dependencies = new_resource.dependencies
   apps_dir = '/srv/shiny-server'
   app_link = ::File.join(apps_dir, name)
-  repositories_dir = '/var/shiny-server/repositories'
-  repository_dir = ::File.join(repositories_dir, name)
   if !dependencies.nil?
     dependencies.each do |dependency|
       r_package dependency
     end
   end
-  if sub_folder.nil?
-    app_dir = repository_dir
-  else
-    app_dir = ::File.join(repository_dir, sub_folder)
-  end
   directory repository_dir do
     recursive true
   end
-  git repository_dir do
-    repository repository
-  end
   link app_link do
-    to app_dir
+    to path
   end
 end
